d1c06e58ce21477288f6453e7e3d01f390c7341a
Thomas Trivino <th992984@ucf.edu>
2025-06-25 14:55:06 -0700

    Adds functionality to navigation buttons on dashboard

--- frontend/src/Patient/pages/PatientDashboardPageComponents/GoToUploadMedicationButton.jsx
+++ frontend/src/Patient/pages/PatientDashboardPageComponents/GoToUploadMedicationButton.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router";
+
 function GoToUploadMedicationButton() {
+    const navigate = useNavigate();
+
     return (
         <>
-            <button className="btn btn-primary btn-lg w-100 p-4 mb-3">Upload Medication</button>
+            <button className="btn btn-primary btn-lg w-100 p-4 mb-3" onClick={() => navigate('/medication-upload')}>Upload Medication</button>
         </>
     );
 }
--- frontend/src/components/GoToAppointmentManagerButton.jsx
+++ frontend/src/components/GoToAppointmentManagerButton.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router";
+
 function GoToAppointmentManagerButton() {
+    const navigate = useNavigate();
+
     return(
         <>
-            <button className="btn btn-light w-100 p-3 mb-3">Go To Appointment Manager</button>
+            <button className="btn btn-light w-100 p-3 mb-3" onClick={() => navigate('/appointment-manager')}>Go To Appointment Manager</button>
         </>
     );
 }
