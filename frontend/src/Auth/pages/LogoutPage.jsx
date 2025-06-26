127ff3d1383a15ee54300267e4661dde98c7f278
Thomas Trivino <th992984@ucf.edu>
2025-06-26 15:28:25 -0700

    Implements basic logout page

--- frontend/src/Auth/pages/LogoutPage.jsx
+++ frontend/src/Auth/pages/LogoutPage.jsx
@@ -1,7 +1,12 @@
 function LogoutPage() {
-    return(
-        <>
-        </>
+    const logoutContainerStyles = {
+        height: "90vh"
+    };
+
+    return (
+        <div style={logoutContainerStyles} className="d-flex align-items-center justify-content-center">
+            <h1>You have been successfully logged out of MediScan!</h1>
+        </div>
     );
 }
 
