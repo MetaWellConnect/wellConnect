998be72284a6b8cf96f9c0af7c70e40d261c5fe8
Thomas Trivino <th992984@ucf.edu>
2025-06-27 14:49:51 -0700

    Removes camera capture function as it is currently not used

--- frontend/src/Patient/PatientComponents/MedicationUploadPageComponents/MedicationCamera.jsx
+++ frontend/src/Patient/PatientComponents/MedicationUploadPageComponents/MedicationCamera.jsx
@@ -1,18 +1,10 @@
 import Webcam from 'react-webcam'
-import { useRef, useState, useCallback } from 'react';
 
 function MedicationWebcam() {
-    const webcamRef = useRef(null);
-    const [imgSrc, setImgSrc] = useState(null);
-
-    const capture = useCallback(() => {
-        const imageSource = webcamRef.current.getScreenshot();
-        setImgSrc(imageSource)
-    }, [webcamRef]);
 
     return (
         <div className="overflow-hidden my-3 rounded-3">
-            <Webcam ref={webcamRef}  mirrored style={{objectFit: 'cover'}} />
+            <Webcam mirrored style={{objectFit: 'cover'}} />
         </div>
     );
 }
