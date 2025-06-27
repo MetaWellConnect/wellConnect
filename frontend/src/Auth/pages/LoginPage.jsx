f5728830e82899a9b1b4705a578aa6016de86a12
Thomas Trivino <th992984@ucf.edu>
2025-06-27 10:53:30 -0700

    Adds routing to the Sign Up button. Also makes fields required for the form and includes a stub for the sign in handling function. Also adds functionality to form fields

--- frontend/src/Auth/pages/LoginPage.jsx
+++ frontend/src/Auth/pages/LoginPage.jsx
@@ -1,26 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+
 function LoginPage() {
+    const navigate = useNavigate();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const loginContainerStyles = {
         height: "95.85vh"
     };
 
+    const handlLoginSubmit = () => {
+        console.log("Button works for now!");
+    }
+
     return (
         <div className="container-fluid overflow-hidden">
             <div style={loginContainerStyles} className="row text-center no-padding">
-                <section className="login-page-welcome col text-white bg-primary bg-gradient d-flex flex-column justify-content-center">
+                <section className="login-page-welcome col-sm text-white bg-primary bg-gradient d-flex flex-column justify-content-center">
                     <h1>Welcome to MediScan!</h1>
                     <p>Don't have an account?</p>
-                    <button className="btn btn-light rounded-pill w-25 mx-auto">Sign Up</button>
+                    <button className="btn btn-light rounded-pill w-25 mx-auto" onClick={() => navigate('/register')}>Sign Up</button>
                 </section>
 
-                <form className="col my-auto">
+                <form className="col-sm my-auto" onSubmit={handlLoginSubmit}>
                     <h2>Sign In</h2>
 
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="First name" required />
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Last name" required />
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Email" required />
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Password" required />
+                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="First name" onChange={(e) => setFirstName(e.target.value)} required />
+                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Last name" onChange={(e) => setLastName(e.target.value)} required />
+                    <input type="email" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+                    <input type="password" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
 
-                    <button className="btn btn-primary rounded-pill w-50 p-3">Sign In</button>
+                    <button className="btn btn-primary rounded-pill w-50 p-3" type="submit">Sign In</button>
                 </form>
             </div>
         </div>
