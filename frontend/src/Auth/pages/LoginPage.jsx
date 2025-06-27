310d84c25701f68f4b1f07a98648cf26a8986c75
Thomas Trivino <th992984@ucf.edu>
2025-06-27 11:03:27 -0700

    Removes first and last name fields from form

--- frontend/src/Auth/pages/LoginPage.jsx
+++ frontend/src/Auth/pages/LoginPage.jsx
@@ -4,8 +4,6 @@ import { useNavigate } from "react-router";
 
 function LoginPage() {
     const navigate = useNavigate();
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -21,7 +19,7 @@ function LoginPage() {
         <div className="container-fluid overflow-hidden">
             <div style={loginContainerStyles} className="row text-center no-padding">
                 <section className="login-page-welcome col-sm text-white bg-primary bg-gradient d-flex flex-column justify-content-center">
-                    <h1>Welcome to MediScan!</h1>
+                    <h1>Welcome back to MediScan!</h1>
                     <p>Don't have an account?</p>
                     <button className="btn btn-light rounded-pill w-25 mx-auto" onClick={() => navigate('/register')}>Sign Up</button>
                 </section>
@@ -29,8 +27,6 @@ function LoginPage() {
                 <form className="col-sm my-auto" onSubmit={handlLoginSubmit}>
                     <h2>Sign In</h2>
 
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="First name" onChange={(e) => setFirstName(e.target.value)} required />
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Last name" onChange={(e) => setLastName(e.target.value)} required />
                     <input type="email" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
                     <input type="password" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
 
