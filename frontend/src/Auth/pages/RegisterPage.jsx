763af005fb5b571299f6c5762e253348bdda9ce6
Thomas Trivino <th992984@ucf.edu>
2025-06-27 14:36:52 -0700

    Adds ability to select patient or provider when register/signup

--- frontend/src/Auth/pages/RegisterPage.jsx
+++ frontend/src/Auth/pages/RegisterPage.jsx
@@ -8,9 +8,15 @@ function RegisterPage() {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [accountType, setAccountType] = useState("");
 
     const handleSignUpSubmit = () => {
         console.log("Button works for now!");
+    }
+
+    const AccountTypes = {
+        PATIENT: "Patient",
+        PROVIDER: "Provider"
     }
 
     return (
@@ -26,9 +32,25 @@ function RegisterPage() {
                     <h2>Register</h2>
 
                     <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="First name" onChange={(e) => setFirstName(e.target.value)} required />
-                    <input type="text" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Last name" onChange={(e) => setLastName(e.target.value)} required />
+                    <input type="text" className="rounded-pill form-control w-50 p-3  my-3 mx-auto" placeholder="Last name" onChange={(e) => setLastName(e.target.value)} required />
                     <input type="email" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
-                    <input type="password" className="rounded-pill form-control w-50 p-3 my-3 mx-auto" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+                    <input type="password" className="rounded-pill form-control w-50 p-3  mx-auto" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+
+
+                    <section className="account-type-select w-50 p-3 my-3 mx-auto text-start" name="account-select" onChange={(e) => setAccountType(e.target.value)}>
+
+                        <section className="form-check form-check-inline">
+                            <input type="radio" className="form-check-input" value={AccountTypes.PATIENT} name="account-select" />
+                            <label className="form-check-label">Patient</label>
+                        </section>
+
+                        <section className="form-check form-check-inline">
+                            <input type="radio" className="form-check-input" value={AccountTypes.PROVIDER} name="account-select" />
+                            <label className="form-check-label">Provider</label>
+                        </section>
+                    </section>
+
+
 
                     <button className="btn btn-primary rounded-pill w-50 p-3" type="submit">Sign Up</button>
                 </form>
