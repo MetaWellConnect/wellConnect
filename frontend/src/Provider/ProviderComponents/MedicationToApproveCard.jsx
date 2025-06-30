d4dc63ab7e8d031fe36bbea49bc41b86a6dc4ea4
Thomas Trivino <th992984@ucf.edu>
2025-06-30 11:07:32 -0700

    Adds patient name and image of medication to MedicationToApproveCard

--- frontend/src/Provider/ProviderComponents/MedicationToApproveCard.jsx
+++ frontend/src/Provider/ProviderComponents/MedicationToApproveCard.jsx
@@ -2,11 +2,12 @@ import ApproveMedicationButton from "./ApproveMedicationButton";
 import DenyMedicationButton from "./DenyMedicationButton";
 import ViewTreatmentPlanButton from "./ViewTreatmentPlanButton";
 
-function MedicationToApproveCard({medication}) {
-    return(
+function MedicationToApproveCard({ medication }) {
+    return (
         <div className="bg-body-secondary rounded-3 my-3 p-2">
+            <p>Medication for {medication.patient}</p>
             <h3>{medication.name}</h3>
-            <p>{medication.description}</p>
+            <img src={medication.photoURL} alt={`Photo of ${medication.name}`} />
 
             <section className="medication-card-buttons">
                 <ViewTreatmentPlanButton />
