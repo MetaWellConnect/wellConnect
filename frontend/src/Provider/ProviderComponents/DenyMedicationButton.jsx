103aa8ed9dcfd2d91561c5e3a5837aa7f019a9d9
Thomas Trivino <th992984@ucf.edu>
2025-06-30 11:09:36 -0700

    Adds handle deny medication function to DenyMedicationButton

--- frontend/src/Provider/ProviderComponents/DenyMedicationButton.jsx
+++ frontend/src/Provider/ProviderComponents/DenyMedicationButton.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 export default function DenyMedicationButton() {
+
+  function handleDenyMedication() {
+
+  }
+
   return (
-    <button className="btn btn-danger m-1">Deny</button>
+    <button className="btn btn-danger m-1" onClick={handleDenyMedication}>Deny</button>
   );
 }
