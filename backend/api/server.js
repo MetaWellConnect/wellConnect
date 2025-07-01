ac415ccd28a9783d131852f107587b02f50679f7
Thomas Trivino <th992984@ucf.edu>
2025-07-01 10:34:59 -0700

    Adds /login endpoint to server to handle login requests

--- backend/api/server.js
+++ backend/api/server.js
@@ -9,6 +9,18 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.get('/login', async (req, res, next) => {
+    const { email, password } = req.user;
+
+    if (areCredentialsValid(email, password)) {
+        const token = generateJWT();
+
+        res.cookie('token', token, {httpOnly: true});
+        res.status(200).json({token}); // Returns the token in response, will be removed
+    }
+    res.status(400).json("Invalid credentials!");
+});
+
 server.get('/', async (req, res, next) => {
     res.status(200).json("Hello World!");
 });
