d09b1c2ce170990982ab3fdf0955e3dbd18d6511
Thomas Trivino <th992984@ucf.edu>
2025-07-01 11:24:17 -0700

    Adds registerUser to handle registration of user and adding to database. Also adds /register endpoint to trigger regitration

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -3,6 +3,32 @@ const prisma = new PrismaClient();
 const jwt = require('jsonwebtoken')
 const argon2 = require('argon2')
 
+/**
+ * This function takes the user information and attempts to register it
+ * If the user already exists according to their unique identifier (email) the registration fails
+ * @param {*} user
+ * @returns Boolean that indicates if the user was successfully registered
+ */
+async function registerUser(user) {
+    const user = prisma.user.findUnique({ where: { email: user.email } })
+    if (!user) {
+        passwordHash = argon2.hash(user.password);
+        prisma.user.create({
+            data: {
+                id: user.id,
+                first_name: user.firstName,
+                last_name: user.lastName,
+                email: user.email,
+                passwordHash: passwordHash
+            }
+        });
+
+        return true;
+    }
+
+    return false;
+}
+
 async function areCredentialsValid(email, password) {
     const user = prisma.user.findUnique({ where: { email: email } })
     if (!user) {
@@ -29,5 +55,6 @@ async function generateJWT(email) {
 
 module.exports = {
     areCredentialsValid,
-    generateJWT
+    generateJWT,
+    registerUser
 }
--- backend/api/server.js
+++ backend/api/server.js
@@ -3,12 +3,22 @@ const prisma = new PrismaClient();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const {areCredentialsValid, generateJWT} = require('./auth.js');
+const { areCredentialsValid, generateJWT, registerUser } = require('./auth.js');
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.post('/register', async (req, res, next) => {
+    const { user } = req.body;
+    const registeredSuccessfully = registerUser(user);
+
+    if (registeredSuccessfully) {
+        res.status(200).json("User registered successfully!");
+    }
+    res.status(400).json("User could not be registered!");
+});
 
 server.get('/login', async (req, res, next) => {
     const { email, password } = req.body.user;
@@ -16,8 +26,8 @@ server.get('/login', async (req, res, next) => {
     if (areCredentialsValid(email, password)) {
         const token = generateJWT(email);
 
-        res.cookie('token', token, {httpOnly: true});
-        res.status(200).json({token}); // Returns the token in response, will be removed
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json({ token }); // Returns the token in response, will be removed
     }
     res.status(400).json("Invalid credentials!");
 });
