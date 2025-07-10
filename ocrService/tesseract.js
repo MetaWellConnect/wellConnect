27d7da39db9f9694847ca9ac2f418dd4a6dd97d3
Thomas Trivino <th992984@ucf.edu>
2025-07-10 10:39:50 -0700

    Adds basic preprocessing and ocr process

---
+++ ocrService/tesseract.js
@@ -0,0 +1,19 @@
+import { createWorker } from "tesseract.js";
+import sharp from "sharp";
+const worker = await createWorker('eng');
+
+async function preprocessImage(inputPath, outputPath) {
+    await sharp(inputPath)
+        .autoOrient()
+        .sharpen()
+        .grayscale()
+        .toFile(outputPath)
+}
+
+async function runOCR(filePath) {
+    const { data: { text } } = await worker.recognize(filePath);
+    console.log(text);
+}
+await preprocessImage('./images/med.jpg', './images/medPROCESSED.jpg')
+await runOCR('./images/medPROCESSED.jpg')
+process.exit(1);
