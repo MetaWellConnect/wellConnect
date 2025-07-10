38e318035a91dbbe7914e3e667eebc7e8ccd7faa
Thomas Trivino <th992984@ucf.edu>
2025-07-10 15:16:28 -0700

    Adds parser.js. Calls on ollama server to use llama3.2:3b to extract info from ocr text

--- ocrService/tesseract.js
+++ ocrService/tesseract.js
@@ -1,5 +1,6 @@
 import { createWorker } from "tesseract.js";
 import sharp from "sharp";
+import { parseOCRText } from "./parser.js";
 const worker = await createWorker('eng');
 
 async function preprocessImage(inputPath, outputPath) {
@@ -7,14 +8,23 @@ async function preprocessImage(inputPath, outputPath) {
         .autoOrient()
         .sharpen()
         .grayscale()
+        .threshold(200)
         .toFile(outputPath)
 }
 
 async function runOCR(filePath) {
     const { data: { text } } = await worker.recognize(filePath);
     console.log(text);
+    return text;
 }
 
-await preprocessImage('./images/med.jpg', './images/medPROCESSED.jpg')
-await runOCR('./images/medPROCESSED.jpg')
+await preprocessImage('./images/advil.jpg', './images/processedImage.jpg')
+const OCR_text = await runOCR('./images/processedImage.jpg');
+
+try {
+    const response = await parseOCRText(OCR_text);
+    console.log(response);
+} catch (e) {
+    console.error(e.message);
+}
 process.exit(1);
