db4df251abd8c2896618429598ab4632c459d925
Thomas Trivino <th992984@ucf.edu>
2025-07-10 13:08:17 -0700

    Adds ollama to docker compose to run phi3 with ollama

--- ocrService/tesseract.js
+++ ocrService/tesseract.js
@@ -14,6 +14,7 @@ async function runOCR(filePath) {
     const { data: { text } } = await worker.recognize(filePath);
     console.log(text);
 }
+
 await preprocessImage('./images/med.jpg', './images/medPROCESSED.jpg')
 await runOCR('./images/medPROCESSED.jpg')
 process.exit(1);
