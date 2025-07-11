f61caf901e2b498270d5af8ca4769a19b533ccc3
Thomas Trivino <th992984@ucf.edu>
2025-07-11 12:53:33 -0700

    Sets up easyOCR as an API service using flask. Containerizes it and adds to docker compose

--- ocrService/easyOCR/easyOCR.py
+++
@@ -1,7 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['en'])
-result = reader.readtext('../uploads/advil.jpg')
-
-for (bbox, text, prob) in result:
-        print(f'Text: {text}, Probability: {prob}')
